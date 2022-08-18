78ddfbe5969d677b03ae98cd193f53472fc52587
EntityKunG <sharpkung4826@gmail.com>
2022-08-18 23:55:42 +0700

    70% of Problem Kayak.

--- Pre/[Pre] Kayak.py
+++ Pre/[Pre] Kayak.py
@@ -2,35 +2,19 @@
 
 def main():
     """Main function"""
-    int(input())
-    x_var = input().split(" ")
-    arr = [float(i) for i in x_var]
-    arr = sorted(arr)
-    arr2 = []
-    for _ in range(0, len(arr) // 2):
-        # find closest
-        var = arr.pop(0)
-        var3 = False
-        diff = 0
-        closestnum = 0
-        for i in range(0, len(arr)):
-            checking = arr[i]
-            if var3 == False:
-                var3 = True
-                diff = abs(checking - var)
-                closestnum = checking
-                continue
-            diff2 = abs(checking - var)
-            if diff2 < diff:
-                closestnum = checking
-                diff = diff2
-        #
-        arr.remove(closestnum)
-        arr2.append(diff)
-    #
-    arr2 = sorted(arr2)
-    arr2.pop(-1)
-    value = sum(arr2)
-    print(str(int(value)))
+    length = int(input())
+    lst = sorted([float(i)for i in input().split()])
+    attemped = []
+    while len(lst) > 2:
+        if lst[1]-lst[0] > lst[2]-lst[1]:
+            attemped.append(lst[2]-lst[1])
+            for _ in range(2):
+                lst.pop(1)
+        else:
+            attemped.append(lst[1]-lst[0])
+            for _ in range(2):
+                lst.pop(0)
+    attemped.append(lst[1]-lst[0])
+    print(int(sum(sorted(attemped)[:length - 1])))
 
 main()
