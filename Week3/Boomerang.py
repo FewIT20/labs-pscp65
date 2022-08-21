8b0c86d06de79c5bb913490c6b8308b97630867c
EntityKunG <sharpkung4826@gmail.com>
2022-08-21 13:53:05 +0700

    Remove call function.

--- Week3/Boomerang.py
+++ Week3/Boomerang.py
@@ -1,8 +1,5 @@
 """IG: fewpz"""
-def call_function_five(x_value, y_value, z_value):
-    """Call this function calutate"""
-    return (y_value - ((y_value**2) - (4*x_value*z_value))**0.5) / (2 * x_value)
- 
+
 def main():
     """Main functions"""
     x_val = int(input())
@@ -13,7 +10,7 @@ def main():
     print(str((7*y_val**3) + (2*y_val**2) - (31*y_val) + 1))
     print(str(-z_val))
     print(str((x_val + y_val) * (x_val - y_val)))
-    print(str(call_function_five(x_val, y_val, z_val)))
+    print(str((y_val - ((y_val**2) - (4*x_val*z_val))**0.5) / (2 * x_val)))
  
  
 main()
