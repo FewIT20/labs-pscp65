07c0f971c522836ec2a7d210b02540f7ddc570da
EntityKunG <sharpkung4826@gmail.com>
2022-09-03 00:13:36 +0700

    Refactor method function to easy read.

--- Week4/DataSpike.py
+++ Week4/DataSpike.py
@@ -1,5 +1,5 @@
 """IG: few.pz"""
-def maimum(var_a, var_b):
+def compare_integer(var_a, var_b):
     """Find the maximum value"""
     if var_a >= var_b:
         return var_a
@@ -8,21 +8,21 @@ def maimum(var_a, var_b):
 
 def main():
     """Main function"""
-    var_1 = int(input())
-    var_2 = int(input())
+    x_val = int(input())
+    y_val = int(input())
 
-    total = maimum(var_1, var_2)
-    var_3 = int(input())
-    total = maimum(total, var_3)
-    var_4 = int(input())
-    total = maimum(total, var_4)
-    var_5 = int(input())
-    total = maimum(total, var_5)
-    var_6 = int(input())
-    total = maimum(total, var_6)
-    var_7 = int(input())
-    total = maimum(total, var_7)
-    var_8 = int(input())
-    total = maimum(total, var_8)
+    total = compare_integer(x_val, y_val)
+    z_val = int(input())
+    total = compare_integer(total, z_val)
+    z_val = int(input())
+    total = compare_integer(total, z_val)
+    z_val = int(input())
+    total = compare_integer(total, z_val)
+    z_val = int(input())
+    total = compare_integer(total, z_val)
+    z_val = int(input())
+    total = compare_integer(total, z_val)
+    z_val = int(input())
+    total = compare_integer(total, z_val)
     print(total)
 main()
